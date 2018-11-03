fe4ecbf168cbb3c276a5e220c3876e2e51b4c3a9
daogster <26748523+daogster@users.noreply.github.com>
2018-11-02 23:27:22 -0500

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteKey: '7591494ad1e56601bc8358580d567b319753bc773de35ce1f0d53bb8e4b97186',
+  siteKey: '16dd4205ec18ce13025f17aeb2d5aa57cdeb90d88a4d143ac60b0e93217d3bd1',
   port: 3002,
   host: 'localhost',
   interval: 1000,
